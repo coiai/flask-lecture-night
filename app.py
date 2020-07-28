302a57255c703d2bf03717f1e3a35d89a46b5c53
coiai <coiaichan@gmail.com>
2020-07-28 20:41:35 +0900

    テンプレートまでやったよ

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,6 +9,21 @@ def hello():
 def top():
     return "ここはトップだよ(๑>◡<๑)"
 
+@app.route('/hello/<text>')
+def namehello(text):
+    return text + "さんこんにちは"
+
+@app.route('/index')
+def index():
+    name = "sunabaco"
+    age = 20
+    address = "田町"
+    return render_template('index.html',user_name = name,user_address = address , user_age = age)
+
+@app.route('/weather/<py_weather>')
+def weather(py_weather):
+
+    return render_template('weather.html',html_weather = py_weather)
 
 if __name__ == '__main__':
     app.run(debug=True)
